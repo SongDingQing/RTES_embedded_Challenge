0ea0dd4c6d67584cd2243a2bbd4e322e64aee560
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2021-12-08 15:49:37 -0500

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,39 @@
 #include <mbed.h>
 
 volatile int flag = 0;
+SPI spi(PF_9, PF_8, PF_7); // mosi, miso, sclk
 DigitalOut cs(PC_1);
-SPI spi(PF_9,PF_8,PF_7);
 
 void setFlag() {          
   flag = 1;
 }
+void setMode() {          
+  cs=0;
+  spi.write(0x20);
+  spi.write(0xCF);
+  cs=1;
+}
 int main() {
   cs=1;
+  setMode();
   spi.format(8,3);
   spi.frequency(100000);
+  
+  
 	Ticker t;
-	t.attach(&setFlag,0.5);
+	t.attach(&setFlag,0.05);
   while(1) {
     if(flag){
       cs=0;
-      spi.write(0xA8);//X_L
-      int xl =spi.write(0x00);
-      if(xl!=0x41){
-        printf("Output on x_l give: 0x%X\n",xl);
-      }
-      cs =1;
+      spi.write(0xE8);//X_L
+      uint8_t xl =spi.write(0x00);
+      cs=1;
+      cs=0;
+      spi.write(0xE9);
+      int8_t xh =spi.write(0x00);
+      cs=1;
+      int16_t data= xh*256+xl-100;
+      printf("Output on x give: %d\n",data);
       flag=0;
     }
     
